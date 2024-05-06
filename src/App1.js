d35b6709fbbcaf481e9b3f56d3cec9fbe437a895
naveenvema <rsnaveenvp@gmail.com>
2024-05-06 20:05:50 +0530

    intial commit

---
+++ src/App1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App1.css"
+
+
+
+export default function App1() {
+  return (
+    <div>
+        <div className='dv'>Hello World</div>
+        <div>Good Afternoon</div>
+            
+    </div>
+
+  )
+}
